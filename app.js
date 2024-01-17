2cd9cb43fc014f6a8da75b0fff25b7676fce0cde
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-17 01:05:00 +0100

    .

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const btns = document.querySelectorAll('.btn')
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         let current = e.currentTarget.classList
-
+        console.log(current)
     })
 })
