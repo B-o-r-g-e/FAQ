451509d54637df2b9abafecb5a06db5f24bff665
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-17 02:54:15 +0100

    .

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ const btns = document.querySelectorAll('.btn')
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        // e.currentTarget.classList.remove('plus')
-        console.log(e.currentTarget.classList)
-        // let current = e.currentTarget
-
-        // if (e.classList.contains('plus')) {
-        //     // current.remove('plus')
-        //     // current.add('show-minus')
-        //     console.log(e)
-        // }
+        if (e.currentTarget.classList.contains('plus')) {
+            e.currentTarget.classList.remove('plus')
+            e.currentTarget.classList.add('show-minus')
+        }
     })
 })
