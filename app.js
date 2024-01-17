4c2b265fc8405310bfe6f39e231b6ebf84e60b13
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-17 03:03:32 +0100

    .

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const btns = document.querySelectorAll('.btn')
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        if (e.currentTarget.classList.contains('plus')) {
-            e.currentTarget.classList.remove('plus')
-            e.currentTarget.classList.add('show-minus')
-        }
+
     })
 })
