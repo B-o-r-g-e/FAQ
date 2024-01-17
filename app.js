f6954342e46f6c540ffe161ac4a355f11dcdb01f
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-17 09:29:19 +0100

    .

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const btns = document.querySelectorAll('.btn')
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-
+        const current = e.currentTarget.classList
+        if (current.contains('plus')) {
+            console.log(e.currentTarget.childElementCount)
+        }
     })
 })
