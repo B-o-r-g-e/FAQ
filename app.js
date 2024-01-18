82a0a0e1926077d33cb54f123f7a323d2ea44993
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-18 06:01:49 +0100

    .

--- app.js
+++ app.js
@@ -26,12 +26,21 @@ const questionContainers = document.querySelectorAll('.question-container')
 
 questionContainers.forEach((questionContainer) => {
     const btn = questionContainer.querySelector('.btn')
+    const btnPlus = questionContainer.querySelector('.btn.plus');
+    const btnMinus = questionContainer.querySelector('.btn.minus');
 
     btn.addEventListener('click', () => {
         questionContainers.forEach((item) => {
             if (item !== questionContainer) {
-
+                item.classList.remove('show-answer')
             }
         })
+
+        // Toggle between showing plus and minus buttons
+        btnPlus.classList.toggle('hide-plus');
+        btnMinus.classList.toggle('show-minus');
+
+        // Toggle the display of the answer
+        answer.classList.toggle('show-answer');
     })
 })
