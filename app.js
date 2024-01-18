078a9e29b5fdb4aef5fa996bedde1cbea0b9d47c
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-18 05:52:09 +0100

    .

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const btns = document.querySelectorAll('.btn');
+// const btns = document.querySelectorAll('.btn');
+//
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', () => {
+//         const questionContainer = btn.closest('.question-container');
+//         const btnPlus = questionContainer.querySelector('.btn.plus');
+//         const btnMinus = questionContainer.querySelector('.btn.minus');
+//         const answer = questionContainer.querySelector('.answer');
+//
+//         // questionContainer.forEach((item) => {
+//         //     btn.addEventListener(() => {
+//         //
+//         //     })
+//         // })
+//
+//         // Toggle between showing plus and minus buttons
+//         btnPlus.classList.toggle('hide-plus');
+//         btnMinus.classList.toggle('show-minus');
+//
+//         // Toggle the display of the answer
+//         answer.classList.toggle('show-answer');
+//     });
+// });
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        const questionContainer = btn.closest('.question-container');
-        const btnPlus = questionContainer.querySelector('.btn.plus');
-        const btnMinus = questionContainer.querySelector('.btn.minus');
-        const answer = questionContainer.querySelector('.answer');
+const questionContainers = document.querySelectorAll('.question-container')
 
-        // questionContainer.forEach((item) => {
-        //     btn.addEventListener(() => {
-        //
-        //     })
-        // })
+questionContainers.forEach((questionContainer) => {
 
-        // Toggle between showing plus and minus buttons
-        btnPlus.classList.toggle('hide-plus');
-        btnMinus.classList.toggle('show-minus');
-
-        // Toggle the display of the answer
-        answer.classList.toggle('show-answer');
-    });
-});
-
-const
+})
